d169846bd44e146fede5fe6f0afef7836181fea3
Adrian Vogelsgesang <avogelsgesang@salesforce.com>
2024-04-26 02:15:43 +0200

    WIP: Support additional overlays in the graph area

---
+++ query-graphs/src/ui/FloatingOverlay.tsx
@@ -0,0 +1,12 @@
+import "./FloatingOverlay.css";
+
+export function FloatingOverlay() {
+    return <div className="react-flow__panel qg-float-overlay">
+        <div className="qg-float-titlebar">
+            JSON plan
+        </div>
+        <div className="qg-float-content">
+            CONTENT!
+        </div>
+    </div>;
+}
